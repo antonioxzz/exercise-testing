3cb1ce6e3c4870d68ed25209cf7a9fd7e78fae71
antonioxzz <antonioitalo.xzz@gmail.com>
2023-05-18 12:47:00 -0500

    starting the test

---
+++ exercise/dic.py
@@ -0,0 +1,7 @@
+"""
+Ejercicio de conteo de palabras: Escribe un programa que lea un archivo 
+de texto y cuente la cantidad de veces que aparece cada palabra en el 
+archivo. 
+Luego, almacena los resultados en un diccionario y muestra los pares 
+palabra-frecuencia en orden alfabético.
+"""
